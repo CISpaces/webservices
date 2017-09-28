9fee24f55b2a6e6b8b7f70641c85426d8cb2a28f
James Graham <J.Graham@software.ac.uk>
2017-09-28 14:00:25 +0100

    Initial commit with servlet template
    Dependencies to follow

---
+++ FEWS/src/main/java/fewsservlet/FEWSServlet.java
@@ -0,0 +1,19 @@
+package fewsservlet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class FEWSServlet {
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World!";
+    }
+
+
+
+}
