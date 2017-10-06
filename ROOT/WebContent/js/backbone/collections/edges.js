99a617465a3f371c864aee108e34e3e02b20bf50
Misol Goh <msg1y15@soton.ac.uk>
2017-10-06 15:32:48 +0100

    headers of Authorization is added

--- ROOT/WebContent/js/backbone/collections/edges.js
+++ ROOT/WebContent/js/backbone/collections/edges.js
@@ -9,13 +9,14 @@ var EdgeList = Backbone.Collection.extend({
   model: app.Edge,
 
   // localStorage: new Backbone.LocalStorage('edges-backbone'),
-  
+  headers: {"Authorization": localStorage.getItem('auth_token')},
+
   url: function() {
       var url = '/VC/rest/edge';
       // if (this.get('edgeID').length > 0) url += '/'+this.get('edgeID');
       return url;
   }
- 
+
 });
 
 app.Edges = new EdgeList();
--- ROOT/WebContent/js/backbone/collections/nodes.js
+++ ROOT/WebContent/js/backbone/collections/nodes.js
@@ -9,13 +9,14 @@ var NodeList = Backbone.Collection.extend({
   model: app.Node,
 
   // localStorage: new Backbone.LocalStorage('nodes-backbone'),
+  headers: {"Authorization": localStorage.getItem('auth_token')},
 
   url: function() {
       var url = '/VC/rest/node';
       // if (this.get('nodeID').length > 0) url += '/'+this.get('nodeID');
       return url;
   }
-  
+
 });
 
 app.Nodes = new NodeList();
