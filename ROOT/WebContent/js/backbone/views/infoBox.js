6f9841b2c463eee9ebba034f120a920cdc63d7ef
Misol Goh <msg1y15@soton.ac.uk>
2017-10-10 16:15:04 +0100

    Add headers of authorization

--- ROOT/WebContent/js/backbone/views/infoBox.js
+++ ROOT/WebContent/js/backbone/views/infoBox.js
@@ -22,6 +22,7 @@ app.InfoBoxView = Backbone.View.extend({
 
 		// Brings a list of topics from FEWS services
 		app.Topics.fetch({
+            headers: {'Authorization': localStorage.getItem('auth_token')},
 			success: function(data){
 				if(data){
 					data.forEach(function(d){
@@ -30,12 +31,13 @@ app.InfoBoxView = Backbone.View.extend({
 				}
 			},
 			error: function(response){
+				console.error("Could not get Topics");
 				console.error(response);
 			}
 		});
 
 		// Brings tweets related to listed topics periodically(30s)
-		var timer = setInterval( "app.infoBoxView.submitTopic()", 30000 );
+		// var timer = setInterval( "app.infoBoxView.submitTopic()", 30000 );
 	},
 
 	render: function(){
@@ -208,6 +210,7 @@ app.InfoBoxView = Backbone.View.extend({
       data: JSON.stringify(topic_list),
 			dataType: 'json',
 			contentType: "application/json",
+            headers: {"Authorization": localStorage.getItem('auth_token')},
 			success: function(result){
           // console.log(result);
 
