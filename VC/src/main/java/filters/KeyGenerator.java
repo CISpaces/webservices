332ecc9546b737e1d07e22d1c0c8e7eaacbb1d39
James Graham <J.Graham@software.ac.uk>
2017-10-13 14:48:22 +0100

    Use properties files for JWT key

--- VC/src/main/java/filters/KeyGenerator.java
+++ VC/src/main/java/filters/KeyGenerator.java
@@ -1,14 +1,51 @@
 package filters;
 
-
 import javax.crypto.spec.SecretKeySpec;
+import java.io.InputStream;
 import java.security.Key;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class KeyGenerator {
+
+    private final static Logger log = Logger.getLogger(KeyGenerator.class.getName());
+
     public static Key generateKey() {
-        //FIXME - This needs to be generated randomly at install time.
-        String keyString = "4egte#;!";
-        Key key = new SecretKeySpec(keyString.getBytes(), 0, keyString.getBytes().length, "DES");
-        return key;
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try{
+
+            ClassLoader classLoader = KeyGenerator.class.getClassLoader();
+            input = classLoader.getResourceAsStream("key.properties");
+            prop.load(input);
+
+            String keyString = prop.getProperty("keyString");
+            log.info("#### Read JWT authentication key from file");
+
+            Key key = new SecretKeySpec(keyString.getBytes(), 0, keyString.getBytes().length, "DES");
+            return key;
+
+        } catch (java.io.IOException exc) {
+
+            log.severe("#### Failed to load JWT key from file" + exc);
+            exc.printStackTrace();
+
+        } finally {
+
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (java.io.IOException exc) {
+                    log.log(Level.SEVERE, "#### Failed to close properties file", exc);
+                    exc.printStackTrace();
+                }
+            }
+
+        }
+
+        return null;
     }
 }
