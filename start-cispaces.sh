e61e03f43d96a4227c734dcc0ce5b1b9922bcf62
John Robinson <j.robinson@software.ac.uk>
2017-11-17 23:23:16 +0000

    Add start, stop and update scripts

---
+++ start-cispaces.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Start Services for CISpaces
+# J.Robinson@software.ac.uk
+
+echo "### - Starting CISPaces "
+source ~/.profile
+
+echo "# - Starting Apache Derby in the background..."
+cd ${CISPACES}/tools/derby
+${GAIAN}/bin/startNetworkServer &
+if [ $? -eq 0 ]; then echo "[OK]"; else echo "[Failed]"; exit; fi
+sleep 2
+cd -
+
+echo
+echo "# - Starting Apache Tomcat in the background..."
+$CATALINA_HOME/bin/startup.sh
+if [ $? -eq 0 ]; then
+	echo "[OK]"
+	echo
+	echo "# Waiting 20s for application startup..."
+	for ((i=20; i>=1; i--))
+	do
+		sleep 1
+    		echo -n "${i}..."
+	done
+	echo
+	echo
+	echo "# CISpaces started listening at the following URLs:"
+	echo
+	ifconfig | grep "inet addr" | cut -d: -f2 | cut -d" " -f1 | while read -r host_ip
+	do
+		echo "http://${host_ip}:8080"
+	done
+	echo
+	echo "Do not close this terminal"
+else echo "[Failed]"; exit; fi
