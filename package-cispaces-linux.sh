d639eec006142bd5fb3d20d60493616bd6d69bdc
John Robinson <j.robinson@software.ac.uk>
2017-12-11 11:35:10 +0000

    New script to package CISpaces web services, along with tomcat and derby

---
+++ package-cispaces-linux.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Package CISpaces WebServices for Deployment on RHEL
+# Tested on RHEL 7.3
+# J.Robinson@software.ac.uk
+
+echo
+echo "### - Package CISpaces Web Services for linux? (Y/n)"
+
+usage="$(basename "$0") [-h] [-y]
+
+where:
+    -h  Show this help text
+    -y  Automatically answer yes for all questions"
+
+yes='false'
+while getopts 'yh' flag; do
+    case "${flag}" in
+        y) yes='true' ;;
+        h) echo "${usage}"
+           exit ;;
+        *) exit 1 ;;
+    esac
+done
+
+if [ "${yes}" != 'true' ]; then read stopgo; if [ "$stopgo" == "n" ]; then exit 0; fi; fi
+
+echo
+echo "Looking for CISpaces Web Services..."
+find cispaces/tools/tomcat/ | grep war$
+if [ $? -ne 0 ]; then
+        echo "# Failed to find CISpaces Web Services to package - did you run 'mvn install'?"
+        exit 1
+fi  
+echo "[OK]"
+
+echo
+echo "Copying in start / stop scripts..."
+cp start-cispaces.sh stop-cispaces.sh cispaces/
+echo "Modifying start / stop scripts..."
+sed -i 's/source \~\/\.profile//' cispaces/start-cispaces.sh cispaces/stop-cispaces.sh \
+	&& sed -i 's/\${CISPACES}\///' cispaces/start-cispaces.sh cispaces/stop-cispaces.sh \
+	&& sed -i 's/\${CATALINA_HOME}\//tools\/tomcat\//' cispaces/start-cispaces.sh cispaces/stop-cispaces.sh
+if [ $? -eq 0 ]; then echo "[OK]"; else echo "[Failed]"; exit; fi
+
+echo
+packagename="cispaces-linux-$(date '+%Y%m%d-%H%M%S').zip"
+echo "Creating archive ${packagename}..."
+zip --symlinks -qr ${packagename} cispaces/
+if [ $? -eq 0 ]; then echo "[OK]"; else echo "[Failed]"; exit; fi
